db1281157c44ba12a12fa9fd484e53bc4983fe38
Rasika-Deodhar <rasikadeodhar95@gmail.com>
2018-09-10 14:39:41 +0530

    users service changed for working on hosted version

--- Admin-Lte/src/app/services/users.service.ts
+++ Admin-Lte/src/app/services/users.service.ts
@@ -7,38 +7,38 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class UsersService {
-  // private apiUrl = 'http://localhost:8016/getAllUsers';
+  // private apiUrl = 'http://10.20.6.36:8016/getAllUsers';
 
   constructor(private httpClient:HttpClient) {
    }
 
    getAllUsers(){
-     return this.httpClient.get('http://localhost:8016/getAllUsers').pipe(
+     return this.httpClient.get('http://10.20.6.36:8016/getAllUsers').pipe(
        map(response=>response)
      );
    }
 
    addUser(user:any){
-      return this.httpClient.post('http://localhost:8016/addUser',user).pipe(
+      return this.httpClient.post('http://10.20.6.36:8016/addUser',user).pipe(
         map(response=>response)
       );
    }
 
    updateUser(user:any){
-      return this.httpClient.post('http://localhost:8016/updateUser',user).pipe(
+      return this.httpClient.post('http://10.20.6.36:8016/updateUser',user).pipe(
         map(response=>response)
       );
    }
 
    deleteUser(userId:number){
-      return this.httpClient.delete('http://localhost:8016/deleteUser/' + userId);
+      return this.httpClient.delete('http://10.20.6.36:8016/deleteUser/' + userId);
       // .pipe(
       //   map(response=>response)
       // );
    }
 
    getUserById(userId:number){
-      return this.httpClient.get('http://localhost:8016/getUserById/' + userId).pipe(
+      return this.httpClient.get('http://10.20.6.36:8016/getUserById/' + userId).pipe(
         map(response=>response)
       );
    }
